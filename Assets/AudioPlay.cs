f59e98d70006fc3a1796ff36c55548ad0384f11d
ArKaDiA_Dev <jfernandorojasc@gmail.com>
2020-04-06 13:51:10 -0500

    MAIN MENU - END MENU - LEVEL SELECTOR

---
+++ Assets/AudioPlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip Hover;
+    bool isHover = false;
+    public AudioClip Click;
+    bool Cliked = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (isHover)
+        //{
+        //    Hover.Pl
+        //}
+    }
+}
---
+++ Assets/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.UI.CoroutineTween;
+public class MenuController : MonoBehaviour
+{
+	// Start is called before the first frame update
+
+
+	public GameObject start;
+	public GameObject Quit;
+	public GameObject Screen;
+	
+
+
+	void Start()
+	{
+		Animator startAnimator = start.GetComponent<Animator>();
+		
+	}
+
+	void Update()
+	{
+		
+	}
+
+
+}
